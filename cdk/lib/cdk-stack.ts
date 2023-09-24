a4cf6f070c935664f95fb1760912e5bbe3ace6ff
Matthew Pagan <mastapegs01@gmail.com>
2023-09-23 21:28:52 -0400

    sample deploy works

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,17 +1,14 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
-import { aws_s3 } from "aws-cdk-lib";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { StaticSite } from "./static-site";
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new StaticSite(this, "StaticSite", {
+      domainName: "matthewpagan.com",
+      siteSubDomain: "example",
+    });
   }
 }
